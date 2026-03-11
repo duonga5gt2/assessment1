595514fc4580bef8ff37bb66631d0b61897c7144
duong <ngoquyduonghn2005@gmail.com>
2026-03-11 18:19:00 +1100

    Day 2 Part 1 Finished

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <cmath>
+#include <sstream>
 //////////////////////////////
 ///
 ///DAY 1
@@ -62,7 +63,7 @@ void dayOnePartOne() { // DAY 1 PART 1
 }
 
 
-void dayOnePartTwo() {
+void dayOnePartTwo() { // DAY 1 PART 2
     std::vector<std::string> moves = read_input_file();
     int start_position = 50;
     int password = 0;
@@ -87,16 +88,69 @@ void dayOnePartTwo() {
 }
 
 
-//////////////////////////////
+
+///////////
 ///
-///DAY 2
+/// DAY 2
 ///
-//////////////////////////////
+/// ///////
+
+std::vector<std::pair<long, long>> getInput2() {
+    std::ifstream in("/Users/ngoquyduong/CLionProjects/assessment_real/input2.txt");
+    if (!in.is_open()) {
+        std::cerr << "Cannot open file\n";
+        throw std::runtime_error("Could not open file");
+    }
+
+    std::string line;
+    getline(in, line);   // reads: 1-3,11-19,21-25
+
+    std::stringstream ss(line);
+    std::string token;
+    std::vector<std::pair<long, long>> ranges;
+
+    while (getline(ss, token, ',')) {
+        size_t dashPos = token.find('-');
+
+        long long left = stoll(token.substr(0, dashPos));
+        long long right = stoll(token.substr(dashPos + 1));
+
+        ranges.push_back({left, right});
+    }
+
+    return ranges;
+}
+
+long long dayTwoPartOne() {
+    std::vector<std::pair<long, long>> ranges = getInput2();
+    long long invalid_id_sum;
+    for (int i = 0; i < ranges.size(); i++) {
+        long long left = ranges[i].first;
+        long long right = ranges[i].second;
+
+        for (long long j = left; j <= right; j++) {
+            std::string id = std::to_string(j);
+            int length = id.length();
+            if (length% 2== 1) {
+                continue;
+            }
+            int mid = length / 2;
+            std::string left_id = id.substr(0, mid);
+            std::string right_id = id.substr(mid);
+
+            if (left_id == right_id) {
+                invalid_id_sum = invalid_id_sum + j;
+            }
+        }
+    }
+    return invalid_id_sum;
+
+
+}
 
 int main() {
 
 
-
-    dayOnePartTwo();
+    std::cout << dayTwoPartOne();
     return 0;
 }
